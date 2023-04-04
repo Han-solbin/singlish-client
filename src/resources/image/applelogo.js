5b0cef77e9fff1b39c7194d4dba819e69b7bf9e3
Han-solbin <hansolbin@icloud.com>
2023-04-04 19:55:01 +0900

    :sparkles: Create logo svg components
    Create logo for kakao, apple, singlish.(렛츠고!)

---
+++ src/resources/image/applelogo.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { COLOR } from "../../constants";
+const Applelogo = ({ size = 24, color = COLOR.BLACK }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill={color}
+    stroke="none"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M16.46 5.79l.3.01a5.6 5.6 0 0 1 4.38 2.38c-.1.07-2.62 1.53-2.59 4.57.04 3.63 3.19 4.84 3.22 4.86-.02.08-.5 1.72-1.66 3.41-1 1.46-2.04 2.92-3.67 2.95-1.6.03-2.13-.96-3.96-.96-1.84 0-2.42.93-3.94.99-1.57.06-2.78-1.58-3.78-3.04-2.07-2.98-3.64-8.42-1.53-12.1a5.87 5.87 0 0 1 4.97-3c1.55-.03 3.01 1.04 3.96 1.04.95 0 2.73-1.29 4.6-1.1zM16.78 0a5.3 5.3 0 0 1-1.25 3.83 4.46 4.46 0 0 1-3.56 1.7 5.03 5.03 0 0 1 1.27-3.71A5.38 5.38 0 0 1 16.78 0z" />
+  </svg>
+);
+export default Applelogo;
