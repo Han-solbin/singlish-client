c3d7f11a9be954053abb0e41749abf24e7e9f183
Han-solbin <hansolbin@icloud.com>
2023-04-05 22:19:39 +0900

    :sparkles: Create components and upload resources
    Now check icon has states.

--- src/resources/icon/checkicon.js
+++ src/resources/icon/checkicon.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { COLOR } from "../../constants";
-const Checkicon = ({ size = 24, color = COLOR.GRAY100 }) => (
+const Checkicon = ({
+  size = 24,
+  isChecked = false,
+  checkedColor = COLOR.BRANDCOLOR,
+}) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={size}
     height={size}
     viewBox="0 0 24 24"
-    fill="none"
+    fill={isChecked ? checkedColor : (COLOR.G = WHITE)}
     stroke={color}
     strokeWidth="2"
     strokeLinecap="round"
